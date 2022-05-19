fc7feda5fa0a890e0b09e4f7971c90d620d05506
JoaoTony <joaojosetony2016@gmail.com>
2022-05-19 17:12:11 +0200

    chore: add jest testing-libray

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/', '/next/'],
+  setupFilesAfterEnv: [
+    '<rootDir>/tests/setup-tests.ts',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+};
